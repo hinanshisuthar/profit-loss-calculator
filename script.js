9eaccd08a9f7e8c5c61b9ed50adbe1e252f676c8
Hinanshi Suthar <88201664+hinanshisuthar@users.noreply.github.com>
2021-10-21 17:00:47 +0530

    refactor: added comments

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var headIng = document.querySelector(".heading");
 checkBtn.addEventListener('click',clickHandler); //listen to click event
 
 function clickHandler(){
+  //calculations
   if(Number(buyingPrice.value) > 0 && ( Number(stockVolume.value) > 0 &&  Number(sellingPrice.value) > 0)){
     var totalBuyingPrice = Number(buyingPrice.value) * Number(stockVolume.value);
      var totalSellingPrice = Number(sellingPrice.value) * Number(stockVolume.value);
@@ -25,18 +26,18 @@ function clickHandler(){
      else if(totalBuyingPrice > totalSellingPrice){
          var loss = totalBuyingPrice - totalSellingPrice;
          var lossPercent = ( loss/totalBuyingPrice)*100;
-         result.innerText = "Hey! this trade was not profitable."
-        + " And you lost " + lossPercent + "% . Which is total Rs. " + loss + "." 
+         result.innerText = "Hey! this trade was not profitable." //display message
+        + " And you lost " + lossPercent + "% . Which is total Rs. " + loss + "."  //display profit and loss %tage and values
          if(lossPercent > 50){
-            headIng.style.color = "#DC2626";
-            result.style.color = "#DC2626"
+            headIng.style.color = "#DC2626"; //change color
+            result.style.color = "#DC2626" //change color
          }
      }
      else{
-         result.innerText = "This trade was neither Profitable nor Lossy-making."
+         result.innerText = "This trade was neither Profitable nor Lossy-making." //display message
      }
     }
  else{
-     result.innerText = "Are you trying to fool me here -_- ? Enter values greater than 0"
+     result.innerText = "Are you trying to fool me here -_- ? Enter values greater than 0" //display message
  }
 }
